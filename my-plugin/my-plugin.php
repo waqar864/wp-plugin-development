760d65d71bf3d30080d0ccdada559377ee63aa41
waqar864 <waqarkhan864@gmail.com>
2025-10-06 15:52:13 +0500

    make custom hook

--- my-plugin/my-plugin.php
+++ my-plugin/my-plugin.php
@@ -41,7 +41,16 @@ if(!defined( 'PLUGINPREFIX_DIR_PATH')) {
  * Register the "book" custom post type
  */
 function pluginprefix_setup_post_type() {
-	register_post_type( 'book', ['public' => true ] ); 
+	register_post_type( 'book', array(
+			'labels'      => array(
+				'name'          => __('Books', 'pluginprefix'),
+                'singular_name' => __('book', 'pluginprefix'),
+                'add_new' => __('Add New Book', 'pluginprefix'),
+                'add_new_item' => __('Add New Book', 'pluginprefix'),
+			),
+				'public'      => true,
+				'has_archive' => true,
+		) ); 
 } 
 add_action( 'init', 'pluginprefix_setup_post_type' );
 
